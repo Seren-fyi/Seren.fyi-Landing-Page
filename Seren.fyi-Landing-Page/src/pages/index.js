58bdf46fbc4dbbe828009f6272b61a3af1901d63
pat2903 <103569848+pat2903@users.noreply.github.com>
2023-06-19 14:17:47 +0100

    move stuff around

--- Seren.fyi-Landing-Page/src/pages/index.js
+++ Seren.fyi-Landing-Page/src/pages/index.js
@@ -35,6 +35,7 @@ import Head from "next/head";
 import { useState } from "react";
 import { app } from "../../firebaseConfig";
 import { GetServerSideProps } from "next";
+import Image from 'next/image';
 
 const primaryFeatures = [
   {
@@ -116,6 +117,9 @@ export default function Home() {
       console.log(error);
     }
   }
+
+
+
   return (
     <div className="bg-gray-900">
       <Head>
@@ -127,12 +131,14 @@ export default function Home() {
         />
       </Head>
       <main>
+
         {/* Hero section */}
         <div className="relative isolate overflow-hidden">
+
+          {/* This creates the grid */}
           <svg
             className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-            aria-hidden="true"
-          >
+            aria-hidden="true">
             <defs>
               <pattern
                 id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
@@ -140,89 +146,63 @@ export default function Home() {
                 height={200}
                 x="50%"
                 y={-1}
-                patternUnits="userSpaceOnUse"
-              >
+                patternUnits="userSpaceOnUse">
                 <path d="M.5 200V.5H200" fill="none" />
+
               </pattern>
             </defs>
             <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
               <path
                 d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-                strokeWidth={0}
-              />
+                strokeWidth={0}/>
             </svg>
             <rect
               width="100%"
               height="100%"
               strokeWidth={0}
-              fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
-            />
+              fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"/>
           </svg>
+
+
+          {/* This creates the background blur */}
           <div
-            className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
-            aria-hidden="true"
-          >
+              className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)]
+              lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]" aria-hidden="true">
+
             <div
               className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-10"
               style={{
                 clipPath:
-                  "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
-              }}
-            />
+                  "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, " +
+                    "55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
+              }}/>
           </div>
-          <div className="mx-auto max-w-7xl px-6 pb-8 pt-10 sm:pb-28 lg:flex lg:px-8 lg:pt-40">
-            <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-              <img className="h-16" src="/serenlogo.png" alt="Your Company" />
-              {/* <div className="mt-16 sm:mt-32 lg:mt-16">
-                <a href="#" className="inline-flex space-x-6">
-                  <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
-                    Latest updates
-                  </span>
-                  <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-                    <span>Just shipped v1.0</span>
-                    <ChevronRightIcon
-                      className="h-5 w-5 text-gray-500"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </a>
-              </div> */}
-              <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Unleashing creativity beyond boundaries - powered by AI
-              </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Experience the future of collaboration in a diverse, AI-enhanced
-                platform where your projects thrive and networks expand,
-                transforming ideas into reality.
-              </p>
-              {/* <div className="mt-10 flex items-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-                >
-                  Get started
-                </a>
-                <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-white"
-                >
-                  Live demo <span aria-hidden="true">→</span>
-                </a>
-              </div> */}
-            </div>
-            {/* <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-              <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-                <img
-                  src="/homepage.png"
-                  alt="App screenshot"
-                  width={2432}
-                  height={1442}
-                  className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-                />
+
+          <div className="flex items-center justify-center">
+            <div className="mx-auto max-w-7xl px-6 pb-8 pt-10 sm:pb-28 lg:flex lg:px-8 lg:pt-40">
+              <div className="mx-auto max-w-4xl flex-shrink-0 lg:mx-0 lg:max-w-3xl lg:pt-8">
+                <img className="h-24 mx-auto" src="/serenlogo.png" alt="Seren.fyi Logo" />
+                <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                  Build. Share. Connect.
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                  Filler text
+                </p>
+                <div className="flex">
+                  <button className="mt-8 flex items-center px-6 py-3 text-lg font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600">
+                    Sign up ↗
+                  </button>
+                  <button className="mt-8 flex items-center px-6 py-3 text-lg font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 ml-4">
+                    Learn More ↗
+                  </button>
+                </div>
               </div>
-            </div> */}
+            </div>
           </div>
+
+
         </div>
+
 
         {/* Feature section */}
         <div className="mx-auto mt-16 max-w-7xl px-6 lg:px-8">
